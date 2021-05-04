2c390afaf7cb8b4082a48b62af9a8dbbd5ace896
Liu-Kai-Yang <70627707+Liu-Kai-Yang@users.noreply.github.com>
2021-05-04 11:21:56 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -88,7 +88,7 @@ async def analyze(request):
     img = preprocess_input( np.array([img]) )
     predictions = learn.predict(img)  
     prediction = predictions.argmax()
-    return JSONResponse({'result': str(class[prediction])})
+    return JSONResponse({'result': str(classes[prediction])})
 
 
 if __name__ == '__main__':
